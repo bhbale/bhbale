241fcc46486007eb5be1200efd8730e108d70336
Brian Bale <baleywalla@gmail.com>
2022-09-14 03:39:51 +0100

    added dark mode styles and toggle

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+const bodyTag = document.querySelector("body")
+const darkModeToggle = document.querySelector("a.dark-mode-toggle")
+
+darkModeToggle.addEventListener("click", function() {
+    bodyTag.classList.toggle("dark-mode")
+})
