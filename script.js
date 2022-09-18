16950c636f9bc62cc48e9c95f584041fc564983c
Brian Bale <baleywalla@gmail.com>
2022-09-18 04:06:11 +0100

    added smiley and tiny updates

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const bodyTag = document.querySelector("body")
-const darkModeToggle = document.querySelector("a.dark-mode-toggle")
+const darkModeToggle = document.querySelector(".dark-mode-toggle")
 
 darkModeToggle.addEventListener("click", function() {
     bodyTag.classList.toggle("dark-mode")
